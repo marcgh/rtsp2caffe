0c984c0b4058ddcdc1055563019b4901d7540301
Marc Racicot <racicotm@gmail.com>
2017-06-17 16:03:41 -0400

    removed some pylint warnings

--- rtsp2caffe.py
+++ rtsp2caffe.py
@@ -1,15 +1,12 @@
+# pylint: disable=C0111, C0103, E1101, W0603, C0411, W0702, W0201, C0301, W0602
+
 import numpy as np
 import cv2
 import threading
-import time
-import logging
-import random
 import Queue
+#import caffe
 
 print cv2.__version__
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='(%(threadName)-9s) %(message)s',)
 
 BUF_SIZE = 5
 q = Queue.Queue(BUF_SIZE)
@@ -35,14 +32,12 @@ class ProducerThread(threading.Thread):
                 q.put(frame, block=False)
             except:
                 print 'processing skip'
-                pass
-
             try:
                 qdisp.put(('scaled', frame, 0.4), block=False)
             except:
                 print 'display skip #', skipcount
                 skipcount += 1
-                pass
+
         self.cap.release()
         return
 
@@ -76,31 +71,28 @@ class ConsumerThread(threading.Thread):
 
     def run(self):
         while alive:
-            self.frame = q.get(block=True)
-            frame = cv2.resize(self.frame, (0, 0), fx=0.5, fy=0.5)
+            frame = q.get(block=True)
+            frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
             if self.firstframe:
                 self.hsv = np.zeros_like(frame)
-                self.hsv[...,1] = 255
+                self.hsv[..., 1] = 255
                 self.old_frame = frame
                 self.old_gray = cv2.cvtColor(self.old_frame, cv2.COLOR_BGR2GRAY)
                 self.firstframe = False
             else:
                 self.frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                self.flow  = cv2.calcOpticalFlowFarneback(self.old_gray, self.frame_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-                self.mag, self.ang = cv2.cartToPolar(self.flow[...,0], self.flow[...,1])
-                self.hsv[...,0] = self.ang*180/np.pi/2
-                self.hsv[...,2] = cv2.normalize(self.mag, None, 0, 255, cv2.NORM_MINMAX)
+                self.flow = cv2.calcOpticalFlowFarneback(self.old_gray, self.frame_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+                self.mag, self.ang = cv2.cartToPolar(self.flow[..., 0], self.flow[..., 1])
+                self.hsv[..., 0] = self.ang*180/np.pi/2
+                self.hsv[..., 2] = cv2.normalize(self.mag, None, 0, 255, cv2.NORM_MINMAX)
                 self.bgr = cv2.cvtColor(self.hsv, cv2.COLOR_HSV2BGR)
-
                 try:
                     qdisp.put(('process', self.bgr, 1.0), block=False)
                 except:
                     print 'display skip from process'
-                    pass
         return
 
 if __name__ == '__main__':
-    
     p = ProducerThread(name='producer')
     c = ConsumerThread(name='consumer')
     d = DisplayThread(name='display')
@@ -109,14 +101,12 @@ if __name__ == '__main__':
     d.daemon = True
     d.start()
     p.start()
-    time.sleep(1)
     c.start()
-    
+
     raw_input("")
+
     alive = False
     p.join()
     c.join()
     d.join()
 
-
-    
